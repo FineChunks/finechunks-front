56e6b008eea87914bcb2cb1492260ef6a8837e6a
melian-e <56927295+melian-e@users.noreply.github.com>
2025-04-30 21:39:18 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HttpService } from '@services/http.service';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent implements OnInit {
+  title = 'finechunks-front';
+
+  constructor(private httpService: HttpService) {}
+
+  ngOnInit(): void {
+    this.httpService.helloWorld().subscribe(data => {
+      console.log('my hello world', data);
+    });
+  }
+}

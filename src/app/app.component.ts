fe4b8cdc6014ab11f7b8cac57fd2f46d8c6e4dab
SousMaring <lise.desire@sfr.fr>
2025-05-03 09:52:12 +0200

    feat: basic home html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+<<<<<<< HEAD
+=======
+import { HttpService } from '@services/http.service';
+import { HomeComponent } from './modules/home/home.component';
+>>>>>>> 6d4d7eb (feat: basic home html)
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })

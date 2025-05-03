d27cf8f3bf05e838eb71ab6c3a28bbb0892dce6b
Iémélian RAMBEAU <iemelian.rambeau@student.junia.com>
2025-05-03 18:10:21 +0200

    fix: tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, finechunks-front');
+    expect(compiled.querySelector('router-outlet')).toBeDefined();
   });
 });
